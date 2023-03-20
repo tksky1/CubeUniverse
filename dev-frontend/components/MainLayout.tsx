a17bca3c8ca122217a949066545c48c106527e94
JoeAtStep <1047726015@qq.com>
2023-03-20 18:32:18 +0800

    feat(dev-frontend/styles, dev-frontend/components): update scrollbar style

--- dev-frontend/components/MainLayout.tsx
+++ dev-frontend/components/MainLayout.tsx
@@ -1,24 +1,8 @@
-import { ReactElement } from "react";
-import { Layout, Menu } from "antd";
+import { ReactElement, useRef, useState } from "react";
+import { Menu } from "antd";
 import styles from "@/styles/components/MainLayout.module.scss"
 import { useRouter } from "next/router";
-const {Header, Sider, Content} = Layout;
-
-type SingleLayerEntry = {
-    title: string,
-    path: string
-}
-
-type DoubleLayersEntry = {
-    title: string,
-    childrenEntries: SingleLayerEntry[]
-}
-
-export type Entry = SingleLayerEntry | DoubleLayersEntry;
-
-function isSingleLayerEntry(entry: Entry): entry is SingleLayerEntry {
-    return (entry as SingleLayerEntry).path === undefined;
-}
+import clsx from "clsx";
 
 interface MainLayout {
     children: ReactElement
@@ -71,13 +55,27 @@ let items = [
 
 export default function MainLayout({children}: MainLayout) {
     let router = useRouter();
+    let [isScrolling, setIsScrolling] = useState(false);
+    let timeoutId = useRef<null | ReturnType<typeof setTimeout>>(null);
+    function handleScroll() {
+        setIsScrolling(true);
+        if (timeoutId.current) {
+            clearTimeout(timeoutId.current);
+        }
+        timeoutId.current = setTimeout(() => {
+            setIsScrolling(false);
+        }, 2000);
+    }
     return (
         <div className={styles.outerContainer}>
             <div className={styles.header}>
-                Header
+                CubeUniverse
             </div>
             <div className={styles.bottomContainer}>
-                <div className={styles.sider}>
+                <div 
+                    className={`${styles.sider} ${isScrolling ? styles.scrolling : ""}`} 
+                    onScroll={() => handleScroll()}
+                >
                     <Menu 
                         theme="light"
                         selectedKeys={[router.asPath]}
@@ -90,7 +88,9 @@ export default function MainLayout({children}: MainLayout) {
                         }}
                     />
                 </div>
-                <div className={styles.contentContainer}>
+                <div 
+                    className={`${styles.contentContainer} ${isScrolling ? styles.scrolling : ""}`} 
+                >
                     {children}
                 </div>
             </div>
--- dev-frontend/tailwind.config.js
+++ dev-frontend/tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 }
