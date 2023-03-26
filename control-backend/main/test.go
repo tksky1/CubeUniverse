31c8aa3e589ef08e39ede7e9526bce4b10e531cf
KMSorSMS <yzwliam@126.com>
2023-03-26 11:44:28 +0800

    save change

--- control-backend/main/test.go
+++ control-backend/main/test.go
@@ -1,23 +1,10 @@
 package main
 
-import "control-backend/cubeControl"
-
-func test2() {
-	json, err := cubeControl.ListObjectBucketClaim()
-	if err != nil {
-		panic(err)
-	}
-	out, err := json.Encode()
-	if err != nil {
-		panic(err)
-	}
-	println(string(out))
-	select {}
-}
+// import "control-backend/cubeControl"
 
 // 测试用
 func test() {
-	test2()
+
 	/*
 		req, err := http.NewRequest("GET", cubeControl.CephApiBase+"api/pool?stats=true", nil)
 		if err != nil {
