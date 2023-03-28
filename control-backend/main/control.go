6c32e208668f30f8f04859eb7ba91752b9cff192
KMSorSMS <yzwliam@126.com>
2023-03-28 21:52:05 +0800

    complete all

--- control-backend/main/control.go
+++ control-backend/main/control.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"CubeUniverse/universalFuncs"
+	"context"
 	"control-backend/cubeControl"
 	"control-backend/login-kit/common"
 	"control-backend/login-kit/model"
 	"control-backend/login-kit/util"
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/bcrypt"
 )
+
+// 开启的端口号
+var portaddress string = ":8888"
 
 type User struct {
 	gorm.Model
@@ -21,6 +29,10 @@ type User struct {
 
 func InitUsrAdmin() {
 	db := common.GetDB()
+	if db == nil {
+		fmt.Println("DB not exist")
+		return
+	}
 	name := "Admin"
 	uid := "12345678901"
 	password := "12345678"
@@ -44,15 +56,21 @@ func InitUsrAdmin() {
 
 }
 
-func loginInit() {
+func loginInit(ch1 chan bool) {
 	util.InitConfig()
+	//初始化DB
+	common.InitDB()
 	db := common.GetDB()
+
 	InitUsrAdmin()
 	defer db.Close()
 	var r *gin.Engine = gin.Default()
 	r = CollectRoute(r) //一次性注册完路由
 
 	port := viper.GetString("server.port")
+	//表明初始化完成
+	ch1 <- true
+	//
 	if port != "" {
 		panic(r.Run(":" + port))
 	}
@@ -60,14 +78,41 @@ func loginInit() {
 
 }
 
+// 监视数据库初始化状态
+func watchDB(ch1 chan bool, srv *http.Server) {
+	if ok := <-ch1; ok { //阻塞会一直等待
+		//接收到后就关闭旧的监听
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		srv.Shutdown(ctx)
+		fmt.Println("closed")
+		return
+	} else {
+		fmt.Print("err")
+	}
+}
 func main() {
+	//先开启一个web服务告诉前端需要等待
+	router := gin.Default()
+	router.GET("/api/wait", func(c *gin.Context) {
+		c.String(http.StatusBadRequest, "{\"msg\":\"waiting\"}")
+	})
+
+	srv := &http.Server{
+		Addr:    portaddress,
+		Handler: router,
+	}
+	go srv.ListenAndServe()
+	//建立一个channel来协程通信
+	ch1 := make(chan bool)
 	//在loginInit正式提供dashbord开始前先检测是否数据库被初始化了
 	//开启一个协程来检查
+	go watchDB(ch1, srv)
 
 	//TODO：删除测试内容
 	// test()
 	//只是测试的时候先执行这个，正常情况下应该先执行cubekit的init
-	loginInit()
+	loginInit(ch1)
 
 	//测试websocket发送数据
 
@@ -79,6 +124,6 @@ func main() {
 
 	// 后端内容...
 	//初始化登录，完成路由注册，实现全部服务
-	loginInit()
+	loginInit(ch1)
 
 }
