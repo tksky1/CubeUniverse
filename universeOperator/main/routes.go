b1b0d84dcffd62b9431b2d8fea6852e3dfab6a4b
KMSorSMS <yzwliam@126.com>
2023-04-13 21:48:39 +0800

    fix bug in wsPutGet

--- universeOperator/main/routes.go
+++ universeOperator/main/routes.go
@@ -6,12 +6,12 @@ import (
 )
 
 func CollectRoute(r *gin.Engine) *gin.Engine {
-	r.POST("/osspgdl", impleOPR.PutGetDeleteListObj)
-	r.GET("/osswspgdl", impleOPR.ConstPushGetDeleteList)
+	r.POST("osspgdl", impleOPR.PutGetDeleteListObj)
+	r.GET("osswspgdl", impleOPR.ConstPushGetDeleteList)
 	//传统restful请求
-	r.GET("/oss", impleOPR.OssGet)
-	r.POST("/oss", impleOPR.OssPut)
-	r.DELETE("/oss", impleOPR.OssDelete)
-	r.GET("/osslist", impleOPR.OssList)
+	r.GET("oss", impleOPR.OssGet)
+	r.POST("oss", impleOPR.OssPut)
+	r.DELETE("oss", impleOPR.OssDelete)
+	r.GET("osslist", impleOPR.OssList)
 	return r
 }
